1e986babb718bd9447767d14866abd89ce9b2d02
Austin Chapin <austinchapin04@gmail.com>
2022-02-24 12:58:57 -0800

    Psuedocode functionality written out for functionalQueue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,18 +2,42 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+    // 0: "value"
+  };
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    // value goes to the back of the queue (left)
+
+    // set to back/left/end?
+    storage[0] = value;
+    // if not the first item
+    // increment everything up by 1? the key value
+    // storage keys += 1
+
+
   };
 
   someInstance.dequeue = function() {
+    // item comes out the front (right)
+
+    // check if there are any values
+    // if not return undefined
+    // get value from front
+    // delete value
+    // increment everything up
+    // return value from front
+
   };
 
   someInstance.size = function() {
+    // number of items in someInstance or storage - length? position back minus position front?
+
   };
 
   return someInstance;
 };
+
+
+// new Queue =  {instance of a Queue with functions as properties} {storage}
