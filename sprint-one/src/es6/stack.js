bfa9afd7c745c8951e3b6bb716fbdeba11489887
Austin Chapin <austinchapin04@gmail.com>
2022-02-24 23:25:28 -0800

    Pass all tests for ES6 version of Queue and Stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,28 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
   }
 
+  push(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  }
+
+
+  pop() {
+    this.counter--;
+    if (this.counter < 0) {
+      this.counter = 0;
+    }
+    var returnItem = this.storage[this.counter];
+    delete this.storage[this.counter];
+    return returnItem;
+  }
+
+
+  size() {
+    return this.counter;
+  }
 }
