debccb00178ae6b8434490b0b063d29896559101
Austin Chapin <austinchapin04@gmail.com>
2022-02-24 21:00:21 -0800

    Functional Shared version of Queue using a methods object and 'this' keyword. Passes all tests.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,33 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var someInstance = {};
+  _.extend(someInstance, queueMethods);
+  someInstance.storage = {};
+  someInstance.start = 0;
+  someInstance.end = 0;
+  someInstance.counter = 0;
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function (value) {
+    this.storage[this.end] = value;
+    this.end++;
+    this.counter++;
+  },
+  dequeue: function () {
 
-
+    if (this.counter > 0) {
+      var returnItem = this.storage[this.start];
+      delete this.storage[this.start];
+      this.start++;
+      this.counter--;
+      return returnItem;
+    }
+    return undefined;
+  },
+  size: function () {
+    return this.counter;
+  }
+};
